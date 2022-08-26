d0714dd98480209a0fc979d99ab0503082f63a39
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-08-26 16:38:25 -0300

    [Flavio-Joao-Felix] Projeto Talker Manager

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
+  console.log('Iniciando projeto')
   response.status(HTTP_OK_STATUS).send();
 });
 
