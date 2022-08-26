f4f48ddc8af02a294322037e1919768f8e0bb767
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-08-26 17:41:42 -0300

    feat: requisito 01 - concluído [correção lint]

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 
-
 const app = express();
 app.use(bodyParser.json());
 app.use('/talker', routes);
