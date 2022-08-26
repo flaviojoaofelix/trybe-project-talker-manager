06fc86cecd3d2f2ad480b5ece42edcd1ea99e0c5
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-08-26 17:56:34 -0300

    feat: requisito 02 - concluído

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,21 @@ const fs = require('../utils/fs.utils');
 
 const router = express.Router();
 
+// Requisito 01
 router.get('/', async (req, res) => {
-  const getAllTalkers = await fs.read();
-  res.status(200).json(getAllTalkers);
+  const talkers = await fs.read();
+  res.status(200).json(talkers);
+});
+
+// Requisito 02
+router.get('/:id', async (req, res) => {
+  const talkers = await fs.read();
+  const { id } = req.params;
+
+  const talkerById = talkers.find((talker) => talker.id === Number(id));
+
+  if (talkerById) return res.status(200).json(talkerById);
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 module.exports = router;
